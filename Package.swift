5a6f797d5369a5ce5df8b08ec6be537e246e47a6
Moon Hee Woong <hee89@livetory.co.kr>
2023-04-11 22:49:02 +0900

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+	name: "DLRadioButton",
+	platforms: [
+		.macOS(.v10_15),
+		.iOS(.v11),
+		.tvOS(.v13),
+		.watchOS(.v6)
+	],
+	products: [
+		.library(
+			name: "DLRadioButton",
+			targets: [
+				"DLRadioButton"
+			]
+		)
+	],
+	targets: [
+		.target(
+			name: "DLRadioButton"
+		)
+	]
+)
